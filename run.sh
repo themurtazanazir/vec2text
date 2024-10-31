bd7266690aeb02e8ddac34856c3ee1492803faa6
murtaza <themurtazanazir@gmail.com>
2024-10-31 08:34:45 +0530

    added run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
+export TMPDIR=/data/inversion/temp/
 export HF_HUB_CACHE=/data/inversion/huggingface/hub/
 export HF_HOME=/data/inversion/huggingface/
 export VEC2TEXT_CACHE=/data/inversion/vec2text/
 # export CUDA_LAUNCH_BLOCKING = 1
-export WANDB_DIR=/data/inversion/inersion_vec2text/
-export TMPDIR=/data/inversion/temp/
+export WANDB_DIR=/data/inversion/inversion_vec2text/
 
 python -m pip install .
 
 nohup python vec2text/run.py --per_device_train_batch_size 240\
                     --per_device_eval_batch_size 240\
-                    --max_seq_length 16\
-                    --num_train_epochs 40\
-                    --max_eval_samples 500\
+                    --max_seq_length 64\
+                    --num_train_epochs 100\
+                    --max_eval_samples 1000\
                     --eval_steps 25000\
-                    --warmup_steps 100000\
+                    --warmup_steps 25000\
                     --learning_rate 0.0002\
                     --dataset_name one_million_instructions\
                     --model_name_or_path t5-base\
@@ -23,8 +23,7 @@ nohup python vec2text/run.py --per_device_train_batch_size 240\
                     --lr_scheduler_type constant_with_warmup\
                     --use_frozen_embeddings_as_input 1\
                     --mock_embedder 0\
-                    --use_less_data 1000000\
                     --embedder_model_name meta-llama/Llama-2-7b-hf\
-                    --max_new_tokens 16\
-                    --output_dir /data/inversion/hidden_saves_random_transformed_llama2/\
-                    --exp_group_name llama-random-transformed-clr-fixed &
+                    --max_new_tokens 42\
+                    --output_dir /data/inversion/hidden_saves_random_transformed_llama2_4_bit/\
+                    --exp_group_name llama-random-transformed-4-bit &
