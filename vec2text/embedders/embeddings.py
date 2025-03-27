711742e79aa57efebe1b71dfd7c7f318a952940a
murtaza <themurtazanazir@gmail.com>
2025-03-27 06:58:58 +0530

    fix

--- vec2text/embedders/embeddings.py
+++ vec2text/embedders/embeddings.py
@@ -190,7 +190,7 @@ class TopKToksLogprobsEmbedder(nn.Module):
     def __call__(self, *args, **kwargs):
         topk_logprob, topk_ids = self.get_toks_logprobs(*args, **kwargs)
         bytes_batch = self.convert_toks_to_bytes(topk_ids)
-        return topk_logprob, bytes_batch
+        return {"topk_logprobs": topk_logprob, "bytes_batch": bytes_batch}
 
 
 class TransformedHiddenStateEmbedder(Embedder, ABC):
