1f1461cd7d383462d2808739539db7ac1e9a1aa5
murtaza <themurtazanazir@gmail.com>
2025-04-21 22:32:33 +0530

    updated analysis

--- analysis.py
+++ analysis.py
@@ -119,6 +119,7 @@ def eval_generation_metrics(
 
 
 def generate(trainer, embedder_input_ids, embedder_attention_mask, debug=False):
+    """inputs prompt tokens to generate prompt tokens"""
     logprobs = get_logprobs(
         trainer.model.embedder.model,
         trainer.embedder_tokenizer,
@@ -340,39 +341,40 @@ def get_val_datasets(embedder_tokenizer, experiment, trainer):
 
 if __name__ == '__main__':
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    cmd = "--per_device_train_batch_size 250 --per_device_eval_batch_size 250 --max_seq_length 64 --num_train_epochs 100 --max_eval_samples 1000 --eval_steps 25000 --warmup_steps 25000 --learning_rate 0.0002 --dataset_name one_million_instructions --model_name_or_path t5-base --use_wandb=0 --experiment inversion_from_hidden_states --bf16=1 --embedder_torch_dtype bfloat16 --lr_scheduler_type constant_with_warmup --use_frozen_embeddings_as_input 1 --mock_embedder 1 --embedder_model_name llama2_chat-random_k-alr --max_new_tokens 16 --output_dir /home/mnazir/vec2text/data/test/experiments/llama2_chat-random_k-alr-16-toks-bugfix-4-nodes/ --exp_group_name llama2-chat --extra_tokens 100"
+    with torch.inference_mode():
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        cmd = "--per_device_train_batch_size 25 --per_device_eval_batch_size 25 --max_seq_length 64 --num_train_epochs 100 --max_eval_samples 1000 --eval_steps 25000 --warmup_steps 25000 --learning_rate 0.0002 --dataset_name one_million_instructions --model_name_or_path t5-base --use_wandb=0 --experiment inversion_from_hidden_states --bf16=1 --embedder_torch_dtype bfloat16 --lr_scheduler_type constant_with_warmup --use_frozen_embeddings_as_input 1 --mock_embedder 1 --embedder_model_name llama2_chat-random_k-alr --max_new_tokens 16 --output_dir /home/mnazir/vec2text/data/test/experiments/llama2_chat-random_k-alr-16-toks-bugfix-4-nodes/ --exp_group_name llama2-chat --extra_tokens 100"
 
-    parser = transformers.HfArgumentParser(
-        (ModelArguments, DataArguments, TrainingArguments)
-    )
-    model_args, data_args, training_args = parser.parse_args_into_dataclasses(
-        cmd.split())
-    experiment = experiment_from_args(model_args, data_args, training_args)
-
-    model = experiment.load_model()
-
-    ckpt = experiment._get_checkpoint()
-    # model.embedder.cpu()
-    print("CKPT", ckpt)
-    trainer = experiment.trainer_cls(
-        model=model,
-        data_collator=experiment.get_collator(tokenizer=model.tokenizer),
-        args=experiment.training_args,
-    )
-    trainer._load_from_checkpoint(ckpt)
-    trainer.model.eval()
-
-    val_datasets_dict = get_val_datasets(trainer.embedder_tokenizer, experiment, trainer)
-    metrics = []
-    for key in val_datasets_dict:
-        dl = trainer.get_eval_dataloader(val_datasets_dict[key])
-        out = eval_generation_metrics(trainer, dl)
-        metrics.append(
-            {
-                "ds": key,
-                "metrics": out,
-            }
+        parser = transformers.HfArgumentParser(
+            (ModelArguments, DataArguments, TrainingArguments)
         )
+        model_args, data_args, training_args = parser.parse_args_into_dataclasses(
+            cmd.split())
+        experiment = experiment_from_args(model_args, data_args, training_args)
 
-    print(metrics)
+        model = experiment.load_model()
+
+        ckpt = experiment._get_checkpoint()
+        # model.embedder.cpu()
+        print("CKPT", ckpt)
+        trainer = experiment.trainer_cls(
+            model=model,
+            data_collator=experiment.get_collator(tokenizer=model.tokenizer),
+            args=experiment.training_args,
+        )
+        trainer._load_from_checkpoint(ckpt)
+        trainer.model.eval()
+
+        val_datasets_dict = get_val_datasets(trainer.embedder_tokenizer, experiment, trainer)
+        metrics = []
+        for key in val_datasets_dict:
+            dl = trainer.get_eval_dataloader(val_datasets_dict[key])
+            out = eval_generation_metrics(trainer, dl)
+            metrics.append(
+                {
+                    "ds": key,
+                    "metrics": out,
+                }
+            )
+
+        print(metrics)
