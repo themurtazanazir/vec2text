792bed3499d9f64e6d08844fd5e001dd553a2a89
murtaza <themurtazanazir@gmail.com>
2025-04-23 14:23:40 +0000

    added new analysis script

---
+++ analysis2.py
@@ -0,0 +1,98 @@
+import json
+import nltk
+import torch
+import tqdm
+from typing import Dict, Tuple, List, Optional, Union
+import copy
+from vec2text.utils import dataset_map_multi_worker, get_num_proc
+from vec2text.run_args import DataArguments, ModelArguments, TrainingArguments
+from vec2text.experiments import experiment_from_args
+from vec2text.data_helpers import load_standard_val_datasets
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import transformers
+import os
+
+
+os.environ["TMPDIR"] = "/home/mnazir/vec2text/data/test/temp/"
+os.environ["HF_HOME"] = "/home/mnazir/vec2text/data/test/huggingface/"
+os.environ["HF_HUB_CACHE"] = "/home/mnazir/vec2text/data/test/huggingface/hub/"
+os.environ["VEC2TEXT_CACHE"] = "/home/mnazir/vec2text/data/test/vec2text/"
+os.environ["WANDB_DIR"] = "/home/mnazir/vec2text/data/test/"
+
+nltk.download("punkt_tab")
+
+import torch
+from vec2text.models.inversion_from_hidden_states import InversionFromHiddenStatesModel
+from vec2text.models.config import InversionConfig
+from transformers import AutoTokenizer
+
+# Load the model from a local directory or from the Hub
+model = InversionFromHiddenStatesModel.from_pretrained(
+    "/home/mnazir/vec2text/data/test/experiments/llama2_chat-random_k-alr-16-toks-bugfix-4-nodes"  # or "username/model-name" from Hub
+)
+
+# Move to appropriate device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+model.eval()
+
+def format(system_message, instruction, chat_format):
+    if chat_format:
+        return f"[INST] <<SYS>>\n{system_message}\n<</SYS>>\n {instruction} [/INST]"
+    else:
+        return system_message + "\n\n" + instruction
+def invert(sys, ins, chat_format, model):
+
+    strings = [format(sys, ins, chat_format).strip()]
+    t = model.embedder_tokenizer
+    print(f"{strings=}", flush=True)
+    print(f"{t.padding_side=}")
+    inputs = t(
+        strings,
+        return_tensors="pt",
+        padding="max_length",
+        # max_length=trainer.model.embedder.max_length,
+        max_length=64,
+        truncation=True,
+    )
+    inputs = {f"embedder_{k}": v for k, v in inputs.items()}
+    gen_kwargs = {}#copy.copy(trainer.gen_kwargs)
+    max_length = model.config.max_seq_length
+    gen_kwargs["max_length"] = max_length
+    outputs = model.generate(inputs, generation_kwargs=gen_kwargs)
+    output_strings = model.tokenizer.batch_decode(
+        outputs, skip_special_tokens=True)
+    return output_strings[0]
+
+
+def invert2(sys, ins, chat_format, model):
+    model = model
+    target_llm = model.embedder
+    target_tokenizer = model.embedder_tokenizer
+    inverter_tokenizer = model.tokenizer
+    llm_output_processor = model.embedding_transform
+    inverter = model.encoder_decoder
+    
+    strings = [format(sys, ins, chat_format).strip()]
+    inverter_tokens = inverter_tokenizer(strings, return_tensors="pt")
+    target_tokens = target_tokenizer(strings, return_tensors="pt")
+    target_tokens = {f"embedder_{k}":v for k,v in target_tokens.items()}
+
+    #target_output= target_llm(**target_tokens)
+    embeds, attn_mask = model.embed_and_project(**target_tokens)
+    gen_kwargs = {}#copy.copy(trainer.gen_kwargs)
+    max_length = model.config.max_seq_length
+    gen_kwargs["max_length"] = max_length
+    return inverter_tokenizer.batch_decode(inverter.generate(
+            # required: input embeddings
+            inputs_embeds=embeds,
+            attention_mask=attn_mask,
+            # optional: input IDs (for starting generation).
+            # typically not set unless generating prefixes for
+            # reranking.
+            **gen_kwargs,
+            ))
+
+print(model)
+print(invert("", "reverse this string", True, model))
+print(invert2("", "reverse this string", True, model))
